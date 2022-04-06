11b68b47218c4e4f8353b1a251fa66666d3dbfb0
Brody Losh <brodylosh@gmail.com>
2022-04-05 22:23:36 -0400

    commit tree 89573f2a3db9d98ddc45d910d57f4f498f26710c

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+    return cats;
+}
+
+function appendCat(name) {
+    let newArray = [];
+    newArray = cats.concat(name);
+    return newArray;
+}
+
+function prependCat(name) {
+    let newArray = [...cats];
+    newArray.unshift(name);
+    return newArray;
+}
+
+function removeLastCat() {
+    let newArray = [...cats];
+    newArray.pop();
+    return newArray;
+}
+
+function removeFirstCat() {
+    let newArray = [...cats];
+    newArray.shift();
+    return newArray;
+}
